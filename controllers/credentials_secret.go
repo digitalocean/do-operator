e89fb4a69a73683a43e112c893b51ade2ea2efa0
Gavin Mogan <git@gavinmogan.com>
2024-02-21 22:45:23 -0800

    Add URIs to connection information
    Some applications like to have connection strings which isn't really
    possible to build with the current implementation, so take in the
    cluster connection string, and replace the user information with the
    user

--- controllers/credentials_secret.go
+++ controllers/credentials_secret.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/digitalocean/godo"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"net/url"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -32,8 +34,8 @@ func credentialsSecretForDefaultDBUser(owner client.Object, db *godo.Database) *
 	return secret
 }
 
-func credentialsSecretForDBUser(owner client.Object, user *godo.DatabaseUser) *corev1.Secret {
-	return &corev1.Secret{
+func credentialsSecretForDBUser(db *godo.Database, owner client.Object, user *godo.DatabaseUser) (*corev1.Secret, error) {
+	secret := &corev1.Secret{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "v1",
 			Kind:       "Secret",
@@ -45,7 +47,25 @@ func credentialsSecretForDBUser(owner client.Object, user *godo.DatabaseUser) *c
 		StringData: map[string]string{
 			"username": user.Name,
 			"password": user.Password,
-			// TODO(awg): Construct uri and private_uri from DB info.
 		},
 	}
+	if db.Connection != nil {
+		dbUri, err := url.Parse(db.Connection.URI)
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse connection uri: %s", err)
+		}
+		dbUri.User = url.UserPassword(user.Name, user.Password)
+		secret.StringData["uri"] = dbUri.String()
+	}
+
+	if db.PrivateConnection != nil {
+		dbPrivateUri, err := url.Parse(db.PrivateConnection.URI)
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse private connection uri: %s", err)
+		}
+		dbPrivateUri.User = url.UserPassword(user.Name, user.Password)
+		secret.StringData["private_uri"] = dbPrivateUri.String()
+	}
+
+	return secret, nil
 }
