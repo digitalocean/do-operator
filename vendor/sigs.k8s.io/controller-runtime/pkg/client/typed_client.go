75a4ff748d7e898286a08fb2cdac957cf4bcac41
pyadagiri <pyadagiri@digitalocean.com>
2026-09-11 10:57:33 +0530

    Bump Go to 1.27 and refresh deps for k8s 1.37

--- vendor/sigs.k8s.io/controller-runtime/pkg/client/typed_client.go
+++ vendor/sigs.k8s.io/controller-runtime/pkg/client/typed_client.go
@@ -18,15 +18,15 @@ package client
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/json"
 	"k8s.io/client-go/util/apply"
 )
 
 var _ Reader = &typedClient{}
-var _ Writer = &typedClient{}
 
 type typedClient struct {
 	resources  *clientRestResources
@@ -73,22 +73,35 @@ func (c *typedClient) Update(ctx context.Context, obj Object, opts ...UpdateOpti
 }
 
 // Delete implements client.Client.
-func (c *typedClient) Delete(ctx context.Context, obj Object, opts ...DeleteOption) error {
+func (c *typedClient) Delete(ctx context.Context, obj Object, opts ...DeleteOption) (*unstructured.Unstructured, error) {
 	o, err := c.resources.getObjMeta(obj)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	deleteOpts := DeleteOptions{}
 	deleteOpts.ApplyOptions(opts)
 
-	return o.Delete().
+	// directly deserializing into unstructured fails in the decoder
+	runtimeObj, err := o.Delete().
 		NamespaceIfScoped(o.namespace, o.isNamespaced()).
 		Resource(o.resource()).
 		Name(o.name).
 		Body(deleteOpts.AsDeleteOptions()).
 		Do(ctx).
-		Error()
+		Get()
+	if err != nil {
+		return nil, err
+	}
+	data, err := json.Marshal(runtimeObj)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal delete response: %w", err)
+	}
+	response := &unstructured.Unstructured{}
+	if err := json.Unmarshal(data, &response.Object); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal delete response: %w", err)
+	}
+	return response, nil
 }
 
 // DeleteAllOf implements client.Client.
@@ -147,13 +160,18 @@ func (c *typedClient) Apply(ctx context.Context, obj runtime.ApplyConfiguration,
 	applyOpts := &ApplyOptions{}
 	applyOpts.ApplyOptions(opts)
 
-	var contentType string
-	body, err := req.
+	resp := req.
 		NamespaceIfScoped(o.namespace, o.isNamespaced()).
 		Resource(o.resource()).
 		Name(o.name).
 		VersionedParams(applyOpts.AsPatchOptions(), c.paramCodec).
-		Do(ctx).
+		Do(ctx)
+	if err := resp.Error(); err != nil {
+		return err
+	}
+
+	var contentType string
+	body, err := resp.
 		ContentType(&contentType).
 		Raw()
 	if err != nil {
@@ -332,14 +350,19 @@ func (c *typedClient) ApplySubResource(ctx context.Context, obj runtime.ApplyCon
 		return fmt.Errorf("failed to create apply request: %w", err)
 	}
 
-	var contentType string
-	respBody, err := req.
+	resp := req.
 		NamespaceIfScoped(o.namespace, o.isNamespaced()).
 		Resource(o.resource()).
 		Name(o.name).
 		SubResource(subResource).
 		VersionedParams(applyOpts.AsPatchOptions(), c.paramCodec).
-		Do(ctx).
+		Do(ctx)
+	if err := resp.Error(); err != nil {
+		return err
+	}
+
+	var contentType string
+	respBody, err := resp.
 		ContentType(&contentType).
 		Raw()
 	if err != nil {
